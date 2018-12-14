4e5622c145656934e1fe1ed78bfc1bbd9622f815
Francisco Javier Puga Castro <fjpuga@aldaba.es>
2018-12-14 11:11:47 +0100

    Comit Inicial

---
+++ GlobalizationTestWinForms/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace GlobalizationTestWinForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.archivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lenguajeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.inglésToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.francésToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.españolToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ediciónToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            resources.ApplyResources(this.menuStrip1, "menuStrip1");
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.archivoToolStripMenuItem,
+            this.ediciónToolStripMenuItem});
+            this.menuStrip1.Name = "menuStrip1";
+            // 
+            // archivoToolStripMenuItem
+            // 
+            resources.ApplyResources(this.archivoToolStripMenuItem, "archivoToolStripMenuItem");
+            this.archivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lenguajeToolStripMenuItem});
+            this.archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
+            // 
+            // lenguajeToolStripMenuItem
+            // 
+            resources.ApplyResources(this.lenguajeToolStripMenuItem, "lenguajeToolStripMenuItem");
+            this.lenguajeToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.inglésToolStripMenuItem,
+            this.francésToolStripMenuItem,
+            this.españolToolStripMenuItem});
+            this.lenguajeToolStripMenuItem.Name = "lenguajeToolStripMenuItem";
+            // 
+            // inglésToolStripMenuItem
+            // 
+            resources.ApplyResources(this.inglésToolStripMenuItem, "inglésToolStripMenuItem");
+            this.inglésToolStripMenuItem.Name = "inglésToolStripMenuItem";
+            this.inglésToolStripMenuItem.Click += new System.EventHandler(this.inglésToolStripMenuItem_Click);
+            // 
+            // francésToolStripMenuItem
+            // 
+            resources.ApplyResources(this.francésToolStripMenuItem, "francésToolStripMenuItem");
+            this.francésToolStripMenuItem.Name = "francésToolStripMenuItem";
+            this.francésToolStripMenuItem.Click += new System.EventHandler(this.francésToolStripMenuItem_Click_1);
+            // 
+            // españolToolStripMenuItem
+            // 
+            resources.ApplyResources(this.españolToolStripMenuItem, "españolToolStripMenuItem");
+            this.españolToolStripMenuItem.Name = "españolToolStripMenuItem";
+            this.españolToolStripMenuItem.Click += new System.EventHandler(this.españolToolStripMenuItem_Click_1);
+            // 
+            // ediciónToolStripMenuItem
+            // 
+            resources.ApplyResources(this.ediciónToolStripMenuItem, "ediciónToolStripMenuItem");
+            this.ediciónToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1});
+            this.ediciónToolStripMenuItem.Name = "ediciónToolStripMenuItem";
+            // 
+            // toolStripMenuItem1
+            // 
+            resources.ApplyResources(this.toolStripMenuItem1, "toolStripMenuItem1");
+            this.toolStripMenuItem1.Image = global::GlobalizationTestWinForms.Properties.Resources.excel;
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Click += new System.EventHandler(this.toolStripMenuItem1_Click);
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // textBox1
+            // 
+            resources.ApplyResources(this.textBox1, "textBox1");
+            this.textBox1.Name = "textBox1";
+            // 
+            // textBox2
+            // 
+            resources.ApplyResources(this.textBox2, "textBox2");
+            this.textBox2.Name = "textBox2";
+            // 
+            // textBox3
+            // 
+            resources.ApplyResources(this.textBox3, "textBox3");
+            this.textBox3.Name = "textBox3";
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.KeyPreview = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem archivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem lenguajeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem inglésToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem francésToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem españolToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.ToolStripMenuItem ediciónToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+    }
+}
+
---
+++ GlobalizationTestWinForms/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.ComponentModel;
+using System.Globalization;
+using System.Threading;
+using System.Windows.Forms;
+using Microsoft.Office.Interop.Excel;
+using Excel = Microsoft.Office.Interop.Excel.Application;
+
+namespace GlobalizationTestWinForms
+{
+    public partial class Form1 : Form
+    {
+        public string language = Properties.Settings.Default.Lenguaje;
+        public Form1()
+        {
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo(language);
+            InitializeComponent();
+        }
+
+        private void ChangeLanguage(string lang)
+        {
+            foreach(Control c in this.Controls)
+            {
+                ComponentResourceManager resources = new ComponentResourceManager(typeof(Form1));
+                resources.ApplyResources(c, c.Name, new CultureInfo(lang));
+            }
+        }
+
+        private void inglésToolStripMenuItem_Click(object sender, System.EventArgs e)
+        {
+            language = "en-GB";
+            inglésToolStripMenuItem.Checked = true;
+            francésToolStripMenuItem.Checked = false;
+            españolToolStripMenuItem.Checked = false;
+
+            Properties.Settings.Default.Lenguaje = language;
+            Properties.Settings.Default.Save();
+            ChangeLanguage(language);
+            System.Windows.Forms.Application.Restart();
+        }
+
+        private void francésToolStripMenuItem_Click_1(object sender, System.EventArgs e)
+        {
+            language = "fr-FR";
+            inglésToolStripMenuItem.Checked = false;
+            francésToolStripMenuItem.Checked = true;
+            españolToolStripMenuItem.Checked = false;
+
+            Properties.Settings.Default.Lenguaje = language;
+            Properties.Settings.Default.Save();
+            ChangeLanguage(language);
+            System.Windows.Forms.Application.Restart();
+        }
+
+        private void españolToolStripMenuItem_Click_1(object sender, System.EventArgs e)
+        {
+            language = "es-ES";
+            inglésToolStripMenuItem.Checked = false;
+            francésToolStripMenuItem.Checked = false;
+            españolToolStripMenuItem.Checked = true;
+
+            Properties.Settings.Default.Lenguaje = language;
+            Properties.Settings.Default.Save();
+            ChangeLanguage(language);
+            System.Windows.Forms.Application.Restart();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                if (MessageBox.Show("¿Está seguro que desea cerrar?", "Confirmación", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    this.Close();
+                }
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (e.CloseReason != CloseReason.ApplicationExitCall)
+            {
+                if (MessageBox.Show("¿Está seguro que desea cerrar?", "Confirmación", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    e.Cancel = false;
+                }
+                else
+                    e.Cancel = true;
+            }
+        }
+
+        private void toolStripMenuItem1_Click(object sender, System.EventArgs e)
+        {
+            object opc = Type.Missing;
+
+            // Creamos app excel
+            Excel app = new Excel();
+            // Libro
+            Workbook book = app.Workbooks.Add(opc);
+            app.DisplayAlerts = false;
+            // Hoja
+            Worksheet hoja = new Worksheet();
+            hoja = (Worksheet)book.Sheets.Add(opc, opc, opc, opc);
+            hoja.Activate();
+            hoja.Cells[1, 1] = "Nombre";
+            hoja.Cells[1, 2] = "Apellidos";
+            hoja.Cells[1, 3] = "Teléfono";
+            hoja.Cells[2, 1] = !string.IsNullOrEmpty(this.textBox1.Text) ? this.textBox1.Text : string.Empty;
+            hoja.Cells[2, 2] = !string.IsNullOrEmpty(this.textBox2.Text) ? this.textBox2.Text : string.Empty;
+            hoja.Cells[2, 3] = !string.IsNullOrEmpty(this.textBox3.Text) ? this.textBox3.Text : string.Empty;
+            app.Visible = true;
+        }
+    }
+}
